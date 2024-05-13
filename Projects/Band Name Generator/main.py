a160e2856f819a62205d0a701e9b51a94651e208
Matthew Thompson <matthewthompson1113@gmail.com>
2024-05-13 17:17:17 -0400

    Day 1 + Day 2 completed

---
+++ Projects/Band Name Generator/main.py
@@ -0,0 +1,20 @@
+#1. Create a greeting for your program.
+
+print("Hello! Today we will figure out your band name! \n")
+
+#2. Ask the user for the city that they grew up in.
+
+city = input("Where did you grow up?" + "\n")
+
+#3. Ask the user for the name of a pet.
+
+animalName = input("What is the name of your first pet?" + "\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+combinedName = city + " " + animalName
+
+#5. Make sure the input cursor shows on a new line:
+
+print("Your band name could be " + combinedName + "\n")
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 100Days
+
+This is a project for myself to become a developer. I started school as a CS
+Major and found that I was unable to finish. This is a test to trying again.
+
+### Plan
+
+I want to document my progress and notes and use this README as a way to keep
+myself accountable.
+
+Here to Hoping!
