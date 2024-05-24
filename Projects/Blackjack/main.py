adea176f885c0f768972b22e21b293b17974dd37
Matthew Thompson <matthewthompson1113@gmail.com>
2024-05-24 17:36:20 -0400

    Added replay abilities but found that 11 is still buggy and that user sum is not calculated right for part of the if.

--- Projects/Blackjack/main.py
+++ Projects/Blackjack/main.py
@@ -22,15 +22,16 @@
 
 # TODO
 
-# Create a function that ask if they want to play again. 
-# Check is dealer gets aces. 
-
+# Create a function that ask if they want to play again. | Done but sloppy
+# Check is dealer gets aces. | Have to test
+# Fix the user score inside of the 'n' portion of the if
 
 
 # Hint 1: Go to this website and try out the Blackjack game:
 #   https://games.washingtonpost.com/games/blackjack/
 from art import logo
 import random
+
 
 def get_starter_cards():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -72,11 +73,24 @@ def aces(user_card1, user_card2):
             "You have an ace. You may go over 21 and this will then be counted as 1. "
         )
         return user_card1, user_card2, True
+    
+def create_cardList():
+    user_card1, user_card2, user_total = get_starter_cards()
+    dealer_card = get_card()
+    dealer_total = dealer_card
 
+    card_list = [
+        {
+            "user": [user_card1, user_card2],
+        },
+        {"computer": [dealer_card]},
+    ]
+
+    return card_list, user_total, dealer_total
 
 def blackjack():
+
     hit_question = True
-    aces = ace_check
     recursion_total_user = user_total
     recursion_total_dealer = dealer_total
 
@@ -111,13 +125,13 @@ def blackjack():
                 hit_question = False
 
             elif recursion_total_user > 21:
-                if aces:
+                if ace_check:
                     for num in card_list[0]["user"]:
                         if num == 11:
                             get_index = card_list[0]["user"].index(num)
                             card_list[0]["user"][get_index] = 1
                             recursion_total_user = sum(card_list[0]["user"])
-                            aces = False
+                            ace_check = False
                             print("You went over 21!")
                             print(
                                 f"Your new card hand is: {card_list[0]['user']}, current score: {recursion_total_user}"
@@ -125,7 +139,6 @@ def blackjack():
                             print(
                                 f"Computers hand:  {card_list[-1]['computer']}, current score: {recursion_total_dealer}"
                             )
-                            blackjack()
                 else:
                     print(
                         f"Your final hand: {card_list[0]['user']}, final score: {recursion_total_user}"
@@ -138,6 +151,18 @@ def blackjack():
                     hit_question = False
 
             elif recursion_total_dealer > 21:
+                for num in card_list[-1]["computer"]:
+                        if num == 11:
+                            get_index = card_list[-1]["computer"].index(num)
+                            card_list[-1]["computer"][get_index] = 1
+                            recursion_total_dealer = sum(card_list[-1]["computer"])
+                            print("The computer went over 21!")
+                            print(
+                                f"Your card hand is: {card_list[0]['user']}, current score: {recursion_total_user}"
+                            )
+                            print(
+                                f"Computers new hand:  {card_list[-1]['computer']}, current score: {recursion_total_dealer}"
+                            )
 
                 print(
                     f"Your final hand: {card_list[0]['user']}, final score: {recursion_total_user}"
@@ -161,15 +186,13 @@ def blackjack():
 
         else:
 
-            dealer_new_card = get_card()
-            card_list[-1]["computer"].append(dealer_new_card)
-            recursion_total_dealer = sum(card_list[-1]["computer"])
-            
             if sum(card_list[-1]["computer"]) <= 17:
                 dealer_new_card = get_card()
                 card_list[-1]["computer"].append(dealer_new_card)
                 recursion_total_dealer = sum(card_list[-1]["computer"])
-                print(f"Computer has drawn a card, their hand is now {card_list[-1]['computer']}")
+                print(
+                    f"Computer has drawn a card, their hand is now {card_list[-1]['computer']}"
+                )
 
             if recursion_total_dealer == recursion_total_user:
                 print(
@@ -209,36 +232,35 @@ def blackjack():
                 )
                 print(f"You win!")
                 hit_question = False
-        
 
 
 print(logo)
-user_card1, user_card2, user_total = get_starter_cards()
-dealer_card = get_card()
-dealer_total = dealer_card
 
+continue_flag = True
 
-card_list = [
-    {
-        "user": [user_card1, user_card2],
-    },
-    {"computer": [dealer_card]},
-]
+while continue_flag:
+    card_list, user_total, dealer_total = create_cardList()
 
-print(f"Your cards:  {card_list[0]['user']}, current score: {user_total}")
+    print(f"Your cards: {card_list[0]['user']}, current score: {user_total}")
 
-print(f"Computer's first card: {str(card_list[-1]['computer'])[1:-1]}")
+    print(f"Computer's first card: {str(card_list[-1]['computer'])[1:-1]}")
 
-print(f"Sum = {sum(card_list[0]['user'])}")
+    if user_total == 21:
+        print("YOU WIN!")
+    elif 11 in card_list[0]["user"][:]:
+        user_card1, user_card2, ace_check = aces(card_list[0]['user'][0], card_list[0]['user'][-1])
+        card_list[0]["user"].clear()
+        card_list[0]["user"].append(user_card1)
+        card_list[0]["user"].append(user_card2)
+        blackjack()
+    else:
+        ace_check = False
+        blackjack()
 
-if user_total == 21:
-    print("YOU WIN!")
-elif 11 in card_list[0]["user"][:]:
-    user_card1, user_card2, ace_check = aces(user_card1, user_card2)
-    card_list[0]["user"].clear()
-    card_list[0]["user"].append(user_card1)
-    card_list[0]["user"].append(user_card2)
-    blackjack()
-else:
-    ace_check = False
-    blackjack()
+    while continue_flag:
+        if input("Would you like to play again? Type 'y' or 'n': ") == "y":
+            continue_flag = True
+            break
+        else:
+            print("Thanks for playing. Come again!")
+            continue_flag = False
