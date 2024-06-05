13d15df64958c1c4c68cb2e4932bb9ae16207d00
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-04 23:25:28 -0400

    Created still_has_questions and a while loop to go through all of the questions.

--- Projects/Quiz Game/quiz_brain.py
+++ Projects/Quiz Game/quiz_brain.py
@@ -5,9 +5,18 @@ class QuizBrain:
 
     def __init__(self, question_list) -> None:
         self.question_number = 0
+        self.correct = 0
         self.question_list = question_list
 
+    def still_has_questions(self) -> bool:
+        """
+        A method that returns True if there is still questions,
+        False if there is not questions.
+        """
+        return self.question_number < len(self.question_list)
+
     def next_question(self):
+        """A method that prints the question and checks the users input"""
 
         selected_q = self.question_list[self.question_number]
 
@@ -17,5 +26,12 @@ class QuizBrain:
             f"Q.{self.question_number}: {selected_q.text} (True/False)?: ")
 
         if user_answer == selected_q.answer:
-            return "Correct"
-        return "Failed"
+            self.correct += 1
+            print("You got it right!")
+            print(f"The correct answer was: {selected_q.answer}")
+            print(
+                f"Your current score is: {self.correct}/{self.question_number}"
+                )
+
+        else:
+            pass
