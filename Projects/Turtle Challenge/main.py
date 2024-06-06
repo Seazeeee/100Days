4ee99e84bf0989409225c927e173bd8793cb8314
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-05 23:32:57 -0400

    Second Turtle Challenge Completed

--- Projects/Turtle Challenge/main.py
+++ Projects/Turtle Challenge/main.py
@@ -3,10 +3,17 @@ from turtle import Turtle, Screen
 casper = Turtle()
 
 # First Challenge: Draw a square.
-for _ in range(4):
-    casper.forward(100)
-    casper.right(90)
+# for _ in range(4):
+#     casper.forward(100)
+#     casper.right(90)
+
+# Second Challenge: Dotted Line
+for _ in range(15):
+    casper.forward(10)
+    casper.penup()
+    casper.forward(10)
+    casper.pendown()
 
 
-my_screen = Screen()
-my_screen.exitonclick()
+MY_SCREEN = Screen()
+MY_SCREEN.exitonclick()
