d5ef6a9a2cdf92e908a47f77eeb4ec1c902d0f89
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-05 23:05:09 -0400

    Challenge 1 Done

---
+++ Projects/Turtle Challenge/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+casper = Turtle()
+
+# First Challenge: Draw a square.
+for _ in range(4):
+    casper.forward(100)
+    casper.right(90)
+
+
+my_screen = Screen()
+my_screen.exitonclick()
