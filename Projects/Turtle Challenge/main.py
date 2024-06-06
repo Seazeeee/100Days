3cc7f2314cf0b8bf25c62f8953786836244eda23
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-05 23:59:51 -0400

    Third Challenge Completed

--- Projects/Turtle Challenge/main.py
+++ Projects/Turtle Challenge/main.py
@@ -1,6 +1,8 @@
 from turtle import Turtle, Screen
+import random
 
 casper = Turtle()
+MY_SCREEN = Screen()
 
 # First Challenge: Draw a square.
 # for _ in range(4):
@@ -8,12 +10,28 @@ casper = Turtle()
 #     casper.right(90)
 
 # Second Challenge: Dotted Line
-for _ in range(15):
-    casper.forward(10)
-    casper.penup()
-    casper.forward(10)
-    casper.pendown()
+# for _ in range(15):
+#     casper.forward(10)
+#     casper.penup()
+#     casper.forward(10)
+#     casper.pendown()
 
+# Third Challenge:
+# Draw a triangle, square, pentagon, hexagon, heptagon, octagon,
+# nonagon, and decagon.
 
-MY_SCREEN = Screen()
+side_count = 3
+goal_side = 10
+while side_count < goal_side:
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    MY_SCREEN.colormode(255)
+    casper.color(r, g, b)
+    for _ in range(side_count):
+        angle = 360 / side_count
+
+        casper.forward(100)
+        casper.right(angle)
+    side_count += 1
 MY_SCREEN.exitonclick()
