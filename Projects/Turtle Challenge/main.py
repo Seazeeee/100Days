b7db1ea6a01419c45d5db9cc61d4b0c58b594990
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-07 00:21:22 -0400

    Challenge 5 Completed.

--- Projects/Turtle Challenge/main.py
+++ Projects/Turtle Challenge/main.py
@@ -4,6 +4,15 @@ import random
 casper = Turtle()
 MY_SCREEN = Screen()
 
+
+def random_color():
+    """A method that returns a random RGB tuple."""
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    return (r, g, b)
+
 # First Challenge: Draw a square.
 # for _ in range(4):
 #     casper.forward(100)
@@ -42,18 +51,32 @@ MY_SCREEN = Screen()
 
 # Based on the mathematical object.
 
-for _ in range(200):
+# for _ in range(200):
 
-    angles = [0, 90, 180, 270]
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    MY_SCREEN.colormode(255)
+#     angles = [0, 90, 180, 270]
+#     MY_SCREEN.colormode(255)
+#     casper.color(random_color())
+#     casper.pensize(10)
+#     casper.speed(8)
+#     casper.forward(30)
+#     casper.setheading(random.choice(angles))
 
-    casper.color(r, g, b)
-    casper.pensize(10)
-    casper.speed(8)
-    casper.forward(30)
-    casper.setheading(random.choice(angles))
+# MY_SCREEN.exitonclick()
+
+# Challenge 5:
+# Make a SPirograph
+def draw_spirograph(size_of_gaps):
+    """Draws a spirogrpah with random colors."""
+    for _ in range(int(360/size_of_gaps)):
+        MY_SCREEN.colormode(255)
+        casper.color(random_color())
+        casper.speed("fastest")
+
+        casper.circle(100)
+        casper.setheading(casper.heading() + size_of_gaps)
+
+
+draw_spirograph(5)
+
 
 MY_SCREEN.exitonclick()
