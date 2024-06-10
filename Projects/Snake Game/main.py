e4916c57a7a241e14dbcb0a6ac2715bc8d9b99b2
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-09 23:11:52 -0400

    Finished creating the snake body.

--- Projects/Snake Game/main.py
+++ Projects/Snake Game/main.py
@@ -5,6 +5,15 @@ SCREEN.setup(width=600, height=600)
 SCREEN.bgcolor("black")
 SCREEN.title("Snake Game")
 
+dict_turtles = {}
+cords = [(0,0), (-20, 0), (-40, 0)]
+
+
+for num_index, position in enumerate(cords):
+    dict_turtles[num_index] = Turtle(shape="square")
+    dict_turtles[num_index].penup()
+    dict_turtles[num_index].color("white")
+    dict_turtles[num_index].goto(cords[num_index])
 
 
 SCREEN.exitonclick()
