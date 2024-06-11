dbb5e156c9805fa25b786a05bd4210831982855e
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-10 23:24:28 -0400

    Cleaned up snake class

--- Projects/Snake Game/snake.py
+++ Projects/Snake Game/snake.py
@@ -1,23 +1,36 @@
+"""Module that creates the snakes for the snake game."""
+
 from turtle import Turtle
 
 
-class Snake:
+CORDS = [(0, 0), (-20, 0), (-40, 0)]  # Declaring CORDS variable.
+MOVE_DISTANCE = 20
 
+
+class Snake:
+    """Creates Snakes alongside making them move"""
     def __init__(self):
         self.dict_turtles = {}
-        self.cords = [(0, 0), (-20, 0), (-40, 0)]
+        self.create_snake()
+
+    def create_snake(self):
+        """
+        Generates the starting snake
+        """
         self.index = 0
 
-        for num_index in self.cords:
+        for num_index in CORDS:
             # Create the different turtle methods needed; adds them to a dict.
             self.dict_turtles[self.index] = Turtle(shape="square")
             self.dict_turtles[self.index].penup()
             self.dict_turtles[self.index].color("white")
             self.dict_turtles[self.index].goto(num_index)
             self.index += 1
-        print(self.dict_turtles)
 
     def move(self):
+        """
+        Allows the previous segment to move to the one in front of it
+        """
         for num_turtle in range(len(self.dict_turtles) - 1, 0, -1):
             # Grab the front turtle and its cords.
             forward_turtle = self.dict_turtles[num_turtle - 1]
@@ -25,4 +38,4 @@ class Snake:
             new_y = forward_turtle.ycor()
             # Move the turtle behind to the new cords.
             self.dict_turtles[num_turtle].goto(x=new_x, y=new_y)
-        self.dict_turtles[0].forward(20)
+        self.dict_turtles[0].forward(MOVE_DISTANCE)
