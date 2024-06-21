e9e239c05190bd6e430ee90091c50cc69bcbb075
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-21 15:59:32 -0400

    Letter Project Completion

---
+++ Projects/Mail Merging/main.py
@@ -0,0 +1,30 @@
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+beginning_index = 0
+ending_index = 0
+
+with open(
+    "Projects/Mail Merging/Input/Names/invited_names.txt", encoding="utf-8"
+) as name:
+
+    contents = name.readlines()
+
+    for name in contents:
+        name_needed = str(name)
+
+        with open(
+            "Projects/Mail Merging/Input/Letters/starting_letter.txt",
+            encoding="utf-8",
+            mode="r",
+        ) as letter_template, open(
+            f"Projects/Mail Merging/ReadyToSend/{name_needed}.txt",
+            encoding="utf-8",
+            mode="w",
+        ) as ready_to_send:
+            for line in letter_template:
+                ready_to_send.write(
+                    line.replace("[name]", name_needed.strip())
+                    )
