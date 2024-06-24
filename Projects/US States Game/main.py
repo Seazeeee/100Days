d4102e3132b33c8afcfa1c8fb35e3b1f3acf1366
Matthew Thompson <matthewthompson1113@gmail.com>
2024-06-24 00:10:48 -0400

    Added logic that creates a CSV of all missed states.

--- Projects/US States Game/main.py
+++ Projects/US States Game/main.py
@@ -20,7 +20,7 @@ DATA = df.read_csv("Projects/US States Game/50_states.csv")
 # Create a list of states to verify answer
 STATE_LIST = DATA["state"].to_list()
 
-# Create a indefinite loop for the game to continue
+# Create a loop until the total amount of states are reached.
 while len(correct_guesses) < 50:
     # Print out the correct guess list
     print(correct_guesses)
@@ -28,8 +28,18 @@ while len(correct_guesses) < 50:
     # Re-prompt the user every time it loops.
     question_answer = question_prompt.count_prompt()
 
+    # Verifies that is not "None" because that means cancel is clicked.
+    if question_answer is None:
+
+        # If cancel is clicked we end the loop and turn the game off.
+        break
     # Check is the given answer to question is in our state list.
-    if question_answer.title() in STATE_LIST:
+    elif question_answer.title() in STATE_LIST:
+
+        # Checks for duplicate answers.
+        if question_answer.title() in correct_guesses:
+            question_prompt.wrong()
+            continue
 
         # Calls to write the states name in the given coords.
         WriteStates(question_answer)
@@ -39,19 +49,20 @@ while len(correct_guesses) < 50:
 
     # Checks if answer is not in the state list.
     elif question_answer not in STATE_LIST:
+        # Creating a method to break
+        if question_answer.lower() == "exit":
 
-        # Verifies that is not "None" because that means cancel is clicked.
-        if question_answer is None:
+            break
 
-            # If cancel is clicked we end the loop and turn the game off.
-            turtle.mainloop()
+        # Runs this command tha removes a count so that the correct
+        # score stays.
+        question_prompt.wrong()
 
-        # Else the given answer is just wrong.
-        else:
+        # Continues to re-run through the loop
+        continue
 
-            # Runs this command tha removes a count so that the correct
-            # score stays.
-            question_prompt.wrong()
+# Finds all the missing states from answers
+missing_states = DATA[~DATA["state"].isin(correct_guesses)]["state"]
 
-            # Continues to re-run through the loop
-            continue
+# Takes those states and creates another CSV of "study" states.
+missing_states.to_csv("Projects/US States Game/states_to_learn.csv")
