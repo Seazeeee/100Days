fe1ce03dc9f5f51989e9ec9052cd813fd72db25b
Matthew Thompson <matthewthompson1113@gmail.com>
2024-07-02 22:35:18 -0400

    Basic TkInter functions and commands.

--- Projects/Day 27 - GUI Learning/main.py
+++ Projects/Day 27 - GUI Learning/main.py
@@ -9,5 +9,25 @@ window.minsize(width=500, height=300)
 label = tkinter.Label(text="Test", font=("Arial", 24, "bold"))
 label.pack()
 
+label["text"] = "New Text"
+label.config(text="New Text")
+
+# Button
+
+
+def button_click():
+    label.config(text="I got clicked")
+    print(user_input.get())
+
+
+button = tkinter.Button(text="Click Me", command=button_click)
+button.pack()
+
+
+# Entry
+
+user_input = tkinter.Entry(width=10)
+user_input.pack()
+
 
 window.mainloop()
