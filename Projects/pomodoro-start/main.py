de5b606a17499f3a7085473ebf427fd7f1e963c7
Matthew Thompson <matthewthompson1113@gmail.com>
2024-07-05 23:36:26 -0400

    Finished added extra buttons and text outside of canvas widget

--- Projects/pomodoro-start/main.py
+++ Projects/pomodoro-start/main.py
@@ -22,10 +22,43 @@ window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = tkinter.Label()
+timer_label.config(
+    text="Timer", font=(FONT_NAME, 40, "bold"), background=YELLOW, fg=GREEN
+)
+timer_label.grid(column=1, row=1)
+
+check_label = tkinter.Label()
+check_label.config(fg=GREEN, text="✔", background=YELLOW, font=(FONT_NAME, 20))
+check_label.grid(column=1, row=4)
+
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 photo = tkinter.PhotoImage(file="Projects/pomodoro-start/tomato.png")
 canvas.create_image(100, 112, image=photo)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=2)
+
+start_button = tkinter.Button(
+    text="Start",
+    font=(FONT_NAME, 16),
+    foreground="Black",
+    background=YELLOW,
+    border=0,
+    justify="center",
+)
+
+start_button.grid(column=0, row=3)
+
+reset_button = tkinter.Button(
+    text="Reset",
+    font=(FONT_NAME, 16),
+    foreground="Black",
+    background=YELLOW,
+    border=0,
+    justify="center",
+)
+
+reset_button.grid(column=2, row=3)
+
 
 window.mainloop()
