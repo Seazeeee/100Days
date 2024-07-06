25309cbf8fea2ca7b45bbc0514c7db8b2221130c
Matthew Thompson <matthewthompson1113@gmail.com>
2024-07-05 23:53:48 -0400

    Timing function somewhat working.

--- Projects/pomodoro-start/main.py
+++ Projects/pomodoro-start/main.py
@@ -1,4 +1,5 @@
 import tkinter
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -14,13 +15,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    countdown(5 * 60)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def countdown(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 
 timer_label = tkinter.Label()
 timer_label.config(
@@ -35,7 +53,9 @@ check_label.grid(column=1, row=4)
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 photo = tkinter.PhotoImage(file="Projects/pomodoro-start/tomato.png")
 canvas.create_image(100, 112, image=photo)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
 canvas.grid(column=1, row=2)
 
 start_button = tkinter.Button(
@@ -45,6 +65,7 @@ start_button = tkinter.Button(
     background=YELLOW,
     border=0,
     justify="center",
+    command=start_timer,
 )
 
 start_button.grid(column=0, row=3)
@@ -59,6 +80,5 @@ reset_button = tkinter.Button(
 )
 
 reset_button.grid(column=2, row=3)
-
 
 window.mainloop()
