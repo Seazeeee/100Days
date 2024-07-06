4431e280a6f79d5b4c4b3cdc5bb5c0814d8f5717
Matthew Thompson <matthewthompson1113@gmail.com>
2024-07-06 11:00:14 -0400

    UI is completed

--- Projects/password-manager-start/main.py
+++ Projects/password-manager-start/main.py
@@ -8,12 +8,48 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = tkinter.Canvas(width=200, height=200, highlightthickness=0)
 photo = tkinter.PhotoImage(file="Projects/password-manager-start/logo.png")
 canvas.create_image(100, 100, image=photo)
-canvas.grid(column=1, row=2)
+canvas.grid(column=1, row=0)
 
+# Creation of all labels
+
+website_label = tkinter.Label()
+website_label.config(text="Website:", fg="black")
+website_label.grid(column=0, row=1)
+
+email_label = tkinter.Label()
+email_label.config(text="Email/Username:", fg="black")
+email_label.grid(column=0, row=2)
+
+pass_label = tkinter.Label()
+pass_label.config(text="Password:", fg="black")
+pass_label.grid(column=0, row=3)
+
+# Creation of Entry's
+
+website_entry = tkinter.Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2, sticky="w")
+
+email_entry = tkinter.Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2, sticky="w")
+
+pass_entry = tkinter.Entry(width=21, justify="left")
+pass_entry.grid(column=1, row=3, sticky="w")
+
+# Creation of buttons
+
+generate_button = tkinter.Button(
+    text="Generate Password", fg="black", border=0, width=14
+)
+generate_button.grid(column=2, row=3)
+
+add_button = tkinter.Button(
+    text="Add", fg="black", border=0, justify="center", width=35
+)
+add_button.grid(column=1, row=4, columnspan=2, sticky="w")
 
 window.mainloop()
