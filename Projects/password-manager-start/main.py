44f5f699667d32a28f684551a41cb3a9807acebd
Matthew Thompson <matthewthompson1113@gmail.com>
2024-07-07 23:06:43 -0400

    Password Manager Finished.

--- Projects/password-manager-start/main.py
+++ Projects/password-manager-start/main.py
@@ -1,7 +1,83 @@
 import tkinter
+import pyperclip
+from random import choice, randint, shuffle
 from tkinter import messagebox
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def generate_pass():
+    letters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    pass_letters = [choice(letters) for _ in range(randint(8, 10))]
+    pass_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    pass_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = pass_letters + pass_symbols + pass_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    pass_entry.insert(index=tkinter.END, string=password)
+
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -75,7 +151,7 @@ pass_entry.grid(column=1, row=3, sticky="w")
 # Creation of buttons
 
 generate_button = tkinter.Button(
-    text="Generate Password", fg="black", border=0, width=14
+    text="Generate Password", fg="black", border=0, width=14, command=generate_pass
 )
 generate_button.grid(column=2, row=3)
 
