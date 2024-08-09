0f333712eed881c9132e910aa63e507123b6760d
Matthew Thompson <matthewthompson1113@gmail.com>
2024-08-08 23:46:16 -0400

    Add try, except clause to help catch edge cases

--- Projects/NATO Alphabet/main.py
+++ Projects/NATO Alphabet/main.py
@@ -8,19 +8,21 @@ data = pandas.read_csv("Projects/NATO Alphabet/nato_phonetic_alphabet.csv")
 # Create a dictionary resulting in {"A": "Alfa", "B": "Bravo"} etc.
 data_dict = {code.letter: code.code for (index, code) in data.iterrows()}
 
-# Get the user input.
-USER_WORD = str(input("Enter a word: "))
 
-# Grab the letters from USER_WORD.
-word_letters = [letters for letters in USER_WORD]
+while True:
+    # Get the user input.
+    USER_WORD = str(input("Enter a word: "))
 
-# Verify those letters are in data_dict and then return their value for
-# the given key.
-in_data = [
-    data_dict[letters.upper()]
-    for letters in word_letters
-    if letters.upper() in data_dict.keys()
-]
+    # Grab the letters from USER_WORD.
+    word_letters = [letters for letters in USER_WORD]
+
+    # Verify those letters are in data_dict and then return their value for
+    # the given key.
+    try:
+        in_data = [data_dict[letters.upper()] for letters in word_letters]
+        break
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
 
 # Print given list.
 print(in_data)
